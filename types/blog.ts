f782ae317e183788c9543b511e781093b15bdb98
YuIX918 <yy819po@gmail.com>
2025-10-26 22:36:19 +0900

    Update blog type to include custom fields

--- types/blog.ts
+++ types/blog.ts
@@ -1,6 +1,8 @@
 export type Blog = {
-    id: string;
-    title: string;
-    body: string;
-    publishedAt: string;
+  id: string;
+  title: string;
+  body: string;
+  publishedAt: string;
+  eyecatch?: any; // アイキャッチ用の行を追加
+  category?: any; // カテゴリー用の行を追加
 };
