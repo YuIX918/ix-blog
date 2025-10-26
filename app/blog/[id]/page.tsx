171801734ff46f9cef202ed32e773ffa2f57f2ff
YuIX918 <yy819po@gmail.com>
2025-10-26 14:19:03 +0900

    Fix TypeScript types for production build

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,17 +1,24 @@
 import { client } from "../../../libs/client";
+import type { Blog } from "../../../types/blog"; // 型をインポート
 
-export default async function BlogId({ params }) {
-    const { id } = params;
-    const blog = await client.get({ endpoint: "blogs", contentId: id });
+// paramsに型を指定
+export default async function BlogId({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const { id } = params;
+  // client.getに<...>で型を指定
+  const blog = await client.get<Blog>({ endpoint: "blogs", contentId: id });
 
-    return (
-        <main>
-            <h1>{blog.title}</h1>
-            <div
-                dangerouslySetInnerHTML={{
-                    __html: `${blog.body}`,
-                }}
-            />
-        </main>
-    );
+  return (
+    <main>
+      <h1>{blog.title}</h1>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `${blog.body}`,
+        }}
+      />
+    </main>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,23 @@
 import Link from "next/link";
 import { client } from "../libs/client";
+import type { Blog } from "../types/blog"; // 型をインポート
 
 export default async function Home() {
-    const { contents } = await client.get({ endpoint: "blogs" });
+  // client.getに<...>で型を指定
+  const { contents } = await client.get<{ contents: Blog }>({
+    endpoint: "blogs",
+  });
 
-    return (
-        <div>
-            <h1>ブログ一覧</h1>
-            <ul>
-                {contents.map((blog) => (
-                    <li key={blog.id}>
-                        <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>ブログ一覧</h1>
+      <ul>
+        {contents.map((blog) => (
+          <li key={blog.id}>
+            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
