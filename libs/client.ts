c2c04cb6b453c73d624d6a019a0c227b6fda790f
YuIX918 <yy819po@gmail.com>
2025-10-26 22:49:42 +0900

    Fix final type definition for list response

--- libs/client.ts
+++ libs/client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
-    serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-    apiKey: process.env.MICROCMS_API_KEY,
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN!, // ← 最後に「!」を追加
+  apiKey: process.env.MICROCMS_API_KEY!, // ← こちらも！
 });
